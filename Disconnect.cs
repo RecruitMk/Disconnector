020e03f60f1cb817c81505f1e3f15aa29895e7ff
RecruitMk <88484654+RecruitMk@users.noreply.github.com>
2022-12-03 20:57:06 +0300

    Create Disconnect.cs

---
+++ Disconnect.cs
@@ -0,0 +1,13 @@
+{$CLEO .cs} 
+{$USE Cleo+} 
+0000: NOP 
+:1 
+wait 0 ms 
+if and 
+not Samp.ChatInputOpened() 
+0AB0: key_pressed 18 // left alt
+0AB0: key_pressed 82 // R 
+004D: jump_if_false @1 
+0B28: samp disconnect_with_reason 0@ // set reconnecting gamestate 
+0AA5: call 8535003 num_params 3 pop 3 0 0 0 // terminate the gameprocess(exitgame)
+0002: jump @1
